33644d5448b9db15e94d8bdd5f65f1dc5a421502
dannyb-moveo <dannyb@moveo.co.il>
2024-05-18 18:00:05 +0300

    added new endpoint

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("Hello, World!");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
